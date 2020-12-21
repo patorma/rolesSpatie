2db943dd2483990fb2747869076d35ea5b6cdff7
patorma <pocontreras@ing.ucsc.cl>
2020-12-20 23:00:24 -0300

    se termino de configurar controlador del rol

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleController extends Controller
 {
+    function __construct()
+    {
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:role-create', ['only' => ['create','store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +23,10 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+         // Muestra una coleccion del recurso
+        $roles = Role::orderBy('id','DESC')->paginate(5);
+        return view('roles.index',compact('roles'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -22,8 +35,11 @@ class RoleController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {
-        //
+    {     // Mostramos un formulario para crear nuevos ejemplos
+         // Carpeta.archivo
+         $permission = Permission::get();
+         return view('roles.create',compact('permission'));
+
     }
 
     /**
@@ -34,7 +50,17 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         // Almacenar en la base de datos nuevos recursos 
+         $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+    
+        $role = Role::create(['name' => $request->input('name')]);
+        $role->syncPermissions($request->input('permission'));
+    
+        return redirect()->route('roles.index')
+                        ->with('success','Role created successfully');
     }
 
     /**
@@ -45,7 +71,15 @@ class RoleController extends Controller
      */
     public function show($id)
     {
-        //
+       // Muestra un recurso
+        // Se busca el rol por id
+        $role = Role::find($id);
+        $rolePermissions = Permission::join("role_has_permissions","role_has_permissions.permission_id","=","permissions.id")
+            ->where("role_has_permissions.role_id",$id)
+            ->get();
+    
+        return view('roles.show',compact('role','rolePermissions'));
+
     }
 
     /**
@@ -56,7 +90,14 @@ class RoleController extends Controller
      */
     public function edit($id)
     {
-        //
+         // Muestra un formulario para editar un recurso
+         $role = Role::find($id);
+         $permission = Permission::get();
+         $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+             ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+             ->all();
+     
+         return view('roles.edit',compact('role','permission','rolePermissions'));
     }
 
     /**
@@ -67,8 +108,23 @@ class RoleController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
+    { 
+        // Actualizar un recurso especifico
+        // Buscamor por id el usuario
+        $this->validate($request, [
+            'name' => 'required',
+            'permission' => 'required',
+        ]);
+    
+        $role = Role::find($id);
+        $role->name = $request->input('name');
+        $role->save();
+    
+        $role->syncPermissions($request->input('permission'));
+    
+        return redirect()->route('roles.index')
+                        ->with('success','Role updated successfully');
+        
     }
 
     /**
@@ -79,6 +135,9 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
-        //
+          // Eliminar un recurso
+          DB::table("roles")->where('id',$id)->delete();
+          return redirect()->route('roles.index')
+                          ->with('success','Role deleted successfully');
     }
 }
